cb708bea80d3602d960018c1c84486e833c1fa60
李梦龙 <2573521862@qq.com>
2020-11-07 21:24:32 +0800

    create a basic project

---
+++ src/test/java/com/dragon/PoemLandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoemLandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
